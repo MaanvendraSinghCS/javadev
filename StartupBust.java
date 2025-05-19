bf15f8f1dbcbff78c4940c5a9103f3d8d4f56aff
MaanvendraSinghCS <maanvendra2004@gmail.com>
2025-05-19 19:01:48 +0530

    complpeted game Startup Bust

--- StartupBust.java
+++ StartupBust.java
@@ -1,189 +1,179 @@
 import java.util.*;
+
 public class StartupBust {
-    
+    public static void main(String[] args) {
+        GameHelper helper = new GameHelper();
+        List<StartUp> startUps = helper.putStartup();
+        int guesses = 0;
+
+        while (!startUps.isEmpty()) {
+            String userGuess = helper.getUserInput();
+            guesses++;
+            String result = "Miss";
+
+            for (int i = 0; i < startUps.size(); i++) {
+                StartUp s = startUps.get(i);
+                result = s.chekUserGuess(userGuess);
+                if (result.equals("Hit")) {
+                    break;
+                }
+                if (!s.getLifeStatus()) {
+                    result = "Kill " + s.getName();
+                    startUps.remove(s);
+                    break;
+                }
+            }
+            System.out.println(result);
+        }
+
+        System.out.println("All Startups are busted! You took " + guesses + " guesses.");
+    }
 }
 
-class GameHelper
-{
-    class Coords
-    {
+
+class GameHelper {
+    class Coords {
         public int row;
         public int col;
         public boolean oddOrEven;
-        Coords(int row, int col)
-        {
+
+        Coords(int row, int col) {
             this.row = row;
             this.col = col;
-            oddOrEven = (row*7 + col)%2 == 1;
+            oddOrEven = (row * 7 + col) % 2 == 1;
         }
     }
+
     private static final int GRID_LENGTH = 7;
     private int[][] grid = new int[GRID_LENGTH][GRID_LENGTH];
-
-
     private final Random random = new Random();
-
     private static final int startUpsInGrid = 3;
+    private final Scanner sc = new Scanner(System.in);
 
-    public List<String> makeStartUps()
-    {
+    public List<String> makeStartUps() {
         List<String> StartUps = new ArrayList<>();
-        for(int i = 0; i < startUpsInGrid; i++)
-        {
+        for (int i = 0; i < startUpsInGrid; i++) {
             int length = random.nextInt(3, 6);
             char[] nameCharacters = new char[length];
-            for(int j = 0; j < length; j++)
-            {
-                nameCharacters[j] = (char)(65 + random.nextInt(26));
+            for (int j = 0; j < length; j++) {
+                nameCharacters[j] = (char) (65 + random.nextInt(26));
             }
-            String name = nameCharacters.toString();
+            String name = new String(nameCharacters);  // Fix here
             StartUps.add(name);
         }
         return StartUps;
-
     }
 
-    public void putStartup()
-    {
+    public List<StartUp> putStartup() {
         List<String> names = makeStartUps();
-        for(String name : names)
-        {
+        List<StartUp> placed = new ArrayList<>();
+
+        for (String name : names) {
             int length = name.length();
-            int indexStartUp = random.nextInt(GRID_LENGTH * GRID_LENGTH);
-            int row = indexStartUp / GRID_LENGTH;
-            int col = indexStartUp % GRID_LENGTH;
+            boolean placedFlag = false;
+
+            while (!placedFlag) {
+                int index = random.nextInt(GRID_LENGTH * GRID_LENGTH);
+                int row = index / GRID_LENGTH;
+                int col = index % GRID_LENGTH;
+                Coords c = new Coords(row, col);
+                int direction = checkStartUp(c, length);
+
+                if (direction != 0) {
+                    List<String> loc = new ArrayList<>();
+                    for (int i = 0; i < length; i++) {
+                        int r = c.row, cl = c.col;
+                        switch (direction) {
+                            case 1 -> cl -= i;
+                            case 2 -> r += i;
+                            case 3 -> cl += i;
+                            case 4 -> r -= i;
+                        }
+                        grid[r][cl] = 1;
+                        loc.add("" + (char) ('A' + r) + cl);
+                    }
+                    placed.add(new StartUp(name, loc));
+                    placedFlag = true;
+                }
+            }
         }
+
+        return placed;
     }
 
-    public String getUserInput()
-    {
-        System.out.print("Please enter your guess: ");
-        Scanner sc = new Scanner(System.in);
-        String guess = sc.nextLine();
-        sc.close();
-        return guess;
+    public String getUserInput() {
+        System.out.print("Please enter your guess (e.g. A3): ");
+        return sc.nextLine().trim().toUpperCase();
     }
 
-    public Coords getIndexFromAlpha(String guess)
-    {
-        int r, c;
-        if(guess.length() == 2)
-        {
-            String row = guess.substring(0, 1);
-            String col = guess.substring(1,2);
-
-            r = (int)(row.toCharArray()[0] - 'A');
-            c = (int)(col.toCharArray()[0] - '0');
-            return new Coords(r, c);
-
+    public Coords getIndexFromAlpha(String guess) {
+        if (guess.length() == 2) {
+            int row = guess.charAt(0) - 'A';
+            int col = guess.charAt(1) - '0';
+            if (row >= 0 && row < GRID_LENGTH && col >= 0 && col < GRID_LENGTH) {
+                return new Coords(row, col);
+            }
         }
-        else return null;
-        
+        return null;
     }
-    public int checkStartUp(Coords c, int length)
-    {
-        
-        int direct = 0;
-        if(c.oddOrEven)
-        {
-            //check in vertical for availability
-            if((c.row + length) < GRID_LENGTH)
-            {
-                for(int i = 0; i < length; i++)
-                {
-                    if(grid[c.row + i][c.col] == 1)
-                    {
-                        direct = 0;
-                        break;
-                    }
-                }
-                direct = 2;
+
+    public int checkStartUp(Coords c, int length) {
+        if (c.oddOrEven) {
+            if (c.row + length <= GRID_LENGTH) {
+                for (int i = 0; i < length; i++)
+                    if (grid[c.row + i][c.col] == 1) return 0;
+                return 2;
+            } else if (c.row - length >= 0) {
+                for (int i = 0; i < length; i++)
+                    if (grid[c.row - i][c.col] == 1) return 0;
+                return 4;
             }
-            else if((c.row + GRID_LENGTH - length)%GRID_LENGTH < GRID_LENGTH)
-            {
-                for(int i = 0; i < length; i++)
-                {
-                    if(grid[(c.row + GRID_LENGTH - i)%GRID_LENGTH][c.col] == 1)
-                    {
-                        direct = 0;
-                        break;
-                    }
-                }
-                direct = 4;
+        } else {
+            if (c.col + length <= GRID_LENGTH) {
+                for (int i = 0; i < length; i++)
+                    if (grid[c.row][c.col + i] == 1) return 0;
+                return 3;
+            } else if (c.col - length >= 0) {
+                for (int i = 0; i < length; i++)
+                    if (grid[c.row][c.col - i] == 1) return 0;
+                return 1;
             }
         }
-        else
-        {
-            if((c.col + length) < GRID_LENGTH)
-            {
-                for(int i = 0; i < length; i++)
-                {
-                    if(grid[c.row][c.col + i] == 1)
-                    {
-                        direct = 0;
-                        break;
-                    }
-                }
-                direct = 3;
-            }
-            else if((c.col + GRID_LENGTH - length)%GRID_LENGTH < GRID_LENGTH)
-            {
-                for(int i = 0; i < length; i++)
-                {
-                    if(grid[c.row][(c.col + GRID_LENGTH - length)%GRID_LENGTH] == 1)
-                    {
-                        direct = 0;
-                        break;
-                    }
-                }
-                direct = 1;
-            }
-        }
-        return direct;
+        return 0;
     }
 }
 
-class StartUp
-{
-    private String name;
-    private List<String> location;
+class StartUp {
+    private final String name;
+    private final List<String> location;
     private boolean lifeStatus;
 
-    StartUp(String name, List<String> location)
-    {
+    StartUp(String name, List<String> location) {
         this.name = name;
         this.location = new ArrayList<>(location);
         lifeStatus = true;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return this.name;
     }
-    public List<String> getLocation()
-    {
+
+    public List<String> getLocation() {
         return this.location;
     }
-    public boolean getLifeStatus()
-    {
+
+    public boolean getLifeStatus() {
         return lifeStatus;
     }
 
-    public String chekUserGuess(String guess)
-    {
+    public String chekUserGuess(String guess) {
         String effect = "Miss";
-        if(location.contains(guess) && lifeStatus)
-        {
-            int index = location.indexOf(guess);
+        if (location.contains(guess) && lifeStatus) {
+            location.remove(guess);
             effect = "Hit";
-            location.remove(index);
-            if(location.isEmpty())
-            {
+            if (location.isEmpty()) {
                 lifeStatus = false;
             }
-        }
-        else{
-            return effect;
         }
         return effect;
     }
